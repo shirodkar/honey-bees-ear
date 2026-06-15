8a277eecdd898ada6f150969c25f8ed8f0054f1d
Sachin Shirodkar <sshirodk@redhat.com>
2026-06-15 19:22:37 -0400

    refactored to quarkus

--- backend-api/src/main/java/com/example/honeybees/api/HoneyBeesApplication.java
+++ backend-api/src/main/java/com/example/honeybees/api/HoneyBeesApplication.java
@@ -1,13 +1,9 @@
 package com.example.honeybees.api;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
 /**
  * @author [Your Name]
  * @version [Version Number]
  * @since [Date]
  */
-@ApplicationPath("/api")
-public class HoneyBeesApplication extends Application {
+public class HoneyBeesApplication {
 }
