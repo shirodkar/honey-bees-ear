62573ba0328c20741ee0b02e771e7c433c0102e2
Sachin Shirodkar <sshirodk@redhat.com>
2026-07-07 18:56:12 -0400

    mta

--- backend-api/src/main/java/com/example/honeybees/api/HoneyBeesApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.honeybees.api;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HoneyBeesApplication extends Application {
-}
